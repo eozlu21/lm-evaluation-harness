2105be09a8390ff73dff4dac4266781f92a0db62
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2025-05-01 14:36:46 +0300

    changed task name and fixed task definition bug

--- run_lm_eval.sh
+++ run_lm_eval.sh
@@ -2,7 +2,7 @@
 
 lm_eval --model hf \
         --model_args pretrained=mistralai/Mistral-Small-3.1-24B-Instruct-2503,dtype=float32 \
-        --tasks yks_tyt_2024 \
+        --tasks yks_2024 \
         --num_fewshot 0 \
         --log_samples \
         --output_path logs/ \
