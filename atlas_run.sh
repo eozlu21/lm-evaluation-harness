5a1e21bea32ab7b8e83973705e02a65a98020fd8
apolat21 <apolat21@login03.kuacc.ku.edu.tr>
2025-05-31 12:03:22 +0300

    anything result

--- atlas_run.sh
+++ atlas_run.sh
@@ -2,9 +2,9 @@
 #SBATCH -A users                # Account name
 #SBATCH -p mid                  # Partition (queue) name
 #SBATCH --qos=users             # Quality of service
-#SBATCH --gres=gpu:v100:1       # Request 1 V100 GPU (32GB VRAM)
-#SBATCH -c 16                    # Reduced CPU cores to 8
-#SBATCH --mem=80G               # Reduced RAM to 50GB
+#SBATCH --gres=gpu:rtx_a6000:1       # Request 
+#SBATCH -c 8                    # Reduced CPU cores to 8
+#SBATCH --mem=50G               # Reduced RAM to 50GB
 #SBATCH --time=1-00:00:00       # Max runtime (1 day)
 #SBATCH -o results_%j.txt       # Save output to results_JOBID.txt
 #SBATCH -e errors_%j.txt        # Save errors to errors_JOBID.txt
@@ -33,3 +33,4 @@ pip install torch>=2.0.0 transformers>=4.37.0 accelerate tiktoken einops
 
 # Run the evaluation
 bash run_lm_eval.sh
+
