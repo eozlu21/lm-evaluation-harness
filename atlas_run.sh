8d75594fd5090a1652861c5629dfa096f57b5bca
atlaspolat <apolat21@ku.edu.tr>
2025-05-31 11:48:17 +0300

    gpu changed

--- atlas_run.sh
+++ atlas_run.sh
@@ -2,9 +2,9 @@
 #SBATCH -A users                # Account name
 #SBATCH -p mid                  # Partition (queue) name
 #SBATCH --qos=users             # Quality of service
-#SBATCH --gres=gpu:a100:1       # Request 1 A100 GPU
-#SBATCH -c 8                    # Reduced CPU cores to 8
-#SBATCH --mem=100G              # Reduced RAM to 100GB
+#SBATCH --gres=gpu:v100:1       # Request 1 V100 GPU (32GB VRAM)
+#SBATCH -c 16                    # Reduced CPU cores to 8
+#SBATCH --mem=80G               # Reduced RAM to 50GB
 #SBATCH --time=1-00:00:00       # Max runtime (1 day)
 #SBATCH -o results_%j.txt       # Save output to results_JOBID.txt
 #SBATCH -e errors_%j.txt        # Save errors to errors_JOBID.txt
