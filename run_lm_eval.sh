ddf341699ff5fe01865673102b9cd13ae4f3a68d
atlaspolat <apolat21@ku.edu.tr>
2025-05-31 11:29:01 +0300

    gwen3 test

--- run_lm_eval.sh
+++ run_lm_eval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 lm_eval --model hf \
-        --model_args pretrained=mistralai/Mistral-Small-3.1-24B-Instruct-2503,dtype=float32 \
+        --model_args pretrained=Qwen/Qwen3-8B-Instruct,dtype=float32,thinking_mode=True \
         --tasks yks_2024 \
-        --num_fewshot 0 \
+        --num_fewshot 5 \
         --log_samples \
-        --output_path logs/ \
+        --output_path atlas_logs/ \
         --device cuda:0
